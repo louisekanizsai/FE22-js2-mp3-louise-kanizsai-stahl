ed8c790959fafebe4029fe6b0b7f484dc90a35dd
itsHampus <112483952+itsHampus@users.noreply.github.com>
2023-03-06 11:53:26 +0100

    update

--- js/index.js
+++ js/index.js
@@ -5,12 +5,18 @@ class Products{
     #balance;
     #baseUrl;
     #buyBtn;
+    #balanceArray;
     constructor(){
         this.#baseUrl = 'https://mp3-webbshop-default-rtdb.europe-west1.firebasedatabase.app/';
         this.getFirebase()
         .then(value => {
             this.createProductCards(value);
         })
+        .then(
+            ()=>{
+                this.currentBalance();
+            }
+        )
     }
     async getFirebase() {
         const url = this.#baseUrl + '.json';
@@ -20,7 +26,7 @@ class Products{
     }
     createProductCards(array){
         console.log(array);
-        array.forEach(product => {
+        array.forEach((product, index) => {
             const productCard = document.createElement('div');
             document.getElementById('productContainer').append(productCard);
             this.#img = document.createElement('img');
@@ -31,9 +37,39 @@ class Products{
             this.#price.innerText = product.price + ' kr';
             this.#buyBtn = document.createElement('button');
             this.#buyBtn.innerText = 'Add to cart';
-
+            this.#buyBtn.id = index;
+            console.log(index);
             productCard.append(this.#img, this.#name, this.#price, this.#buyBtn);
+            
+            this.#buyBtn.addEventListener('click', ()=>{
+                this.checkBalance(index);
+            })
+            this.#balance = product.balance;
+            if(this.#balance == 0) {
+                this.#buyBtn.disabled = true;
+            }
         });
+        const buttons = document.querySelectorAll('buttons');
+    }
+    async currentBalance(){
+        const productArray = await this.getFirebase();
+        this.#balanceArray =[];
+        productArray.forEach(
+            product=>{
+                this.#balanceArray.push(product.balance)
+            }
+        )
+    }
+    checkBalance(index){
+        if(this.#balanceArray[index] > 0){
+            this.#balanceArray[index]--;
+
+        }
+        else {
+            document.getElementById(index).disabled = true;
+        }
+        console.log(this.#balanceArray, index);
     }
 }
 const el = new Products();
+el.checkBalance();
